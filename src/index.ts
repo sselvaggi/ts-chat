816d19c7f8cf846929a3f53a2cc97695c13c9a15
Sergio Selvaggi <sergio.selvaggi@gmail.com>
2020-04-17 12:07:05 +0200

    IMPROVED express routing for index.html

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as fs from 'fs';
 import RealtimeHttpServer from './RealtimeHttpServer';
 import Chat from './Chat';
 const server = new RealtimeHttpServer(8081);
 server.io.on(RealtimeHttpServer.CONNECTION, Chat.create);
 server.app.use(express.static(path.join(__dirname, 'public')));
-server.app.get('/', (req, res) => {
-  res.sendfile(path.join(__dirname, 'frontend', 'index.html'))
+server.app.get('/', (req:express.Request, res:express.Response) => {
+  res.setHeader("content-type", "text/html");
+  fs.createReadStream(path.join(__dirname, 'frontend', 'index.html')).pipe(res);
 });
 export default server;
