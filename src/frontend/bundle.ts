e635c0ec8a45fca49fdcc17fdb728e31a4f845d2
Sergio Selvaggi <sergio.selvaggi@gmail.com>
2020-04-18 03:18:06 +0200

    ADDED Types support for JS module (socket.io-stream)

--- src/frontend/bundle.ts
+++ src/frontend/bundle.ts
@@ -1,18 +1,16 @@
-///<reference path="../../node_modules/@types/socket.io-client/index.d.ts"/>
 ///<reference path="types.d.ts"/>
+import sioc from 'socket.io-client';
+import ss from 'socket.io-stream';
 const CHAT_MESSAGE='CHAT_MESSAGE'
-const socket = io();
+const socket = sioc();
 const fileInput: HTMLInputElement = <HTMLInputElement>document.querySelector('#file')!
 fileInput.addEventListener('change', (event: Event) => {
   const inputEvent:HTMLInputEvent = <HTMLInputEvent>event;
   const files: FileList = inputEvent?.target?.files!;
   if (files?.length) {
     const file = files[0];
-    //@ts-ignore //ss
     const stream = ss.createStream();
-    //@ts-ignore //ss
     ss(socket).emit('file', stream, {size: file.size});
-    //@ts-ignore //ss
     ss.createBlobReadStream(file).pipe(stream);
   } else {
     console.log('file not received')
@@ -20,8 +18,7 @@ fileInput.addEventListener('change', (event: Event) => {
 });
 
 const m:HTMLInputElement = <HTMLInputElement>document.querySelector('#m')!
-//@ts-ignore
-document.querySelector('form').addEventListener('submit', event => {
+document.querySelector('form')?.addEventListener('submit', event => {
   event.preventDefault();
   socket.emit(CHAT_MESSAGE, m.value);
   return false;
@@ -29,8 +26,7 @@ document.querySelector('form').addEventListener('submit', event => {
 socket.on(CHAT_MESSAGE, (message:string) => {
   const li = document.createElement('li');
   li.textContent = message;
-  //@ts-ignore
   document.querySelector('#messages')
-  .appendChild(li);
+  ?.appendChild(li);
   window.scrollTo(0, document.body.scrollHeight);
 });
--- src/frontend/types.d.ts
+++ src/frontend/types.d.ts
@@ -1,10 +1,4 @@
-///<reference path="../../node_modules/@types/socket.io-client/index.d.ts"/>
-declare namespace SocketIOClient{
-  // Maybe It will be needed to reimplement ConnectOpts that is omiting parser
-  // export interface ConnectOpts {
-  //   parser?: any
-  // }
-}
+declare module "socket.io-stream";
 interface HTMLInputEvent extends Event {
   target: HTMLInputElement & EventTarget;
 }
